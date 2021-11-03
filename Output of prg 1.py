495b12077ba506b4d5850541e5784e6bb2176776
SumaiyaSultanaRA <93434018+SumaiyaSultanaRA@users.noreply.github.com>
2021-11-03 20:11:07 +0530

    Add files via upload

---
+++ Output of prg 1.py
@@ -0,0 +1,7 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:/Users/USER/Desktop/prg2.py ====================
+Enter the value of radius78
+Enter the value of height23.9
+The area of cone with radius78 and  height 23.9 is 456579
---
+++ Output of prg 2.py
@@ -0,0 +1,21 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:/Users/USER/Desktop/prg2.py ====================
+Enter the value of radius78
+Enter the value of height23.9
+The area of cone with radius78 and  height 23.9 is 456579
+
+==================== RESTART: C:/Users/USER/Desktop/prg3.py ====================
+Enter the value of radius 7
+The area of circle with radius 7 is 153
+Traceback (most recent call last):
+  File "C:/Users/USER/Desktop/prg3.py", line 6, in <module>
+    print("The circumference of the circle with radius {} is {}" .format(radius,str(circumfernce)))
+NameError: name 'circumfernce' is not defined. Did you mean: 'Circumference'?
+
+
+==================== RESTART: C:/Users/USER/Desktop/prg3.py ====================
+Enter the value of radius7
+The area of circle with radius 7 is 153
+The circumference of the circle with radius 7 is 43.96
